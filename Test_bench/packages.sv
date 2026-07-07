f922902eba6ba4ad51910771f66437c3fc14fd03
KrishnaGirish2 <krishnagirish1458@gmail.com>
2026-07-07 06:36:47 +0530

    Add ram_pkg package with multiple includes

---
+++ Test_bench/packages.sv
@@ -0,0 +1,10 @@
+package ram_pkg;
+  `include "ram_transaction1.sv"
+  `include "ram_generator1.sv"
+  `include "ram_driver1.sv"
+  `include "ram_monitor1.sv"
+  `include "ram_reference_model1.sv"
+  `include "ram_scoreboard1.sv"
+  `include "ram_environment1.sv"
+  `include "ram_test1.sv"
+endpackage
